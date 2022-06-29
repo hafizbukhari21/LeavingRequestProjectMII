995120dbaa68264be10860972b13e898c368a8bd
M Hafiz B <hafiz.bukhari@hotmail.com>
2022-06-29 14:47:26 +0700

    Models Create Model draft1

---
+++ LeavingRequestProjectMII/Context/MyContext.cs
@@ -0,0 +1,67 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LeavingRequest>()
+                .HasOne(emp => emp.employees)
+                .WithMany(lr => lr.leavingRequests)
+                .HasForeignKey(lr => lr.employee_id);
+
+            modelBuilder.Entity<LeavingRequest>()
+                .HasOne(lcate => lcate.leaveCategory)
+                .WithMany(lr => lr.leavingRequests)
+                .HasForeignKey(lr => lr.category_id);
+
+            modelBuilder.Entity<Employees>()
+                .HasOne(emp => emp.role)
+                .WithMany(emp => emp.employees)
+                .HasForeignKey(emp => emp.role_Id);
+
+            modelBuilder.Entity<Employees>()
+                .HasOne(emp => emp.divisi)
+                .WithMany(emp => emp.employees)
+                .HasForeignKey(emp => emp.divisi_id);
+
+
+             
+
+               
+        
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                     .UseLazyLoadingProxies();
+
+            }
+
+        }
+
+        public DbSet<Employees> employees { set; get; }
+        public DbSet<Role> roles { set; get; }
+        public DbSet<LeavingRequest> leavingRequests { set; get; }
+        public DbSet<Divisi> divisi { set; get; }
+        public DbSet<LeaveCategory> leaveCategories { set; get; }
+       
+
+
+
+        
+    }
+}
---
+++ LeavingRequestProjectMII/Models/LeaveCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class LeaveCategory
+    {
+
+        public LeaveCategory()
+        {
+            leavingRequests = new HashSet<LeavingRequest>();
+        }
+        [Key]
+        public int category_id { set; get; }
+        public string nameCategory { set; get; }
+        public virtual ICollection<LeavingRequest> leavingRequests { set; get; }
+    }
+}
