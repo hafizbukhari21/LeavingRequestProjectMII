aa73227a6ee7a588033d6325c01381524173add1
Yuda Wirawan <yudawirawan7@gmail.com>
2022-06-30 10:27:46 +0700

    add controller template

---
+++ Client/Controllers/LeaveController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers
+{
+    public class LeaveController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("Add/")]
+        public IActionResult Add()
+        {
+            return View("Add");
+        }
+    }
+}
