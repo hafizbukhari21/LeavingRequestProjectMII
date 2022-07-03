844992aaa7b0866e63f675cd2513e39e83ec9767
Yuda Wirawan <yudawirawan7@gmail.com>
2022-07-03 21:02:57 +0700

    update leave req

--- Client/wwwroot/js/leaveRequestConfirmation.js
+++ Client/wwwroot/js/leaveRequestConfirmation.js
@@ -4,15 +4,15 @@
 $(document).ready(function () {
 
     let man = {
-        "manager_id": "Employee0007"
+        manager_id: "Employee0007"
     }
 
     $('#tblMan').DataTable({
         ajax: {
-            url: "https://localhost:44302/api/leavingrequest/man",
-            type: "POST",
-            "contentType": "application/json",
-            "data": JSON.stringify(man)
+            //url: `https://localhost:44302/api/leavingrequest/man/${idEmp}`,
+            url: `https://localhost:44302/api/leavingrequest/man/Employee0007`,
+            "dataType": "json",
+            "dataSrc": "",
         },
         dom: 'lBfrtip',
         buttons: [
@@ -26,7 +26,7 @@ $(document).ready(function () {
             {
                 extend: 'excel',
                 className: 'btn btn-success btn-sm',
-                text: '<i class="far fa-file-excel"> </i>',
+                text: '<i class="fas fa-file-excel"> </i>',
                 titleAttr: 'Download to excel'
             },
             {
@@ -74,7 +74,7 @@ $(document).ready(function () {
                 data: "request_id",
                 render: function (data, type, row, meta) {
                     return `<div class="btn-group text-center">
-                                <button type="button" class="btn btn-sm btn-warning" title="Delete" onClick="employeeDetail('Employee/Get/${data}')" data-toggle="modal" data-target="#updateModal">
+                                <button type="button" class="btn btn-sm btn-warning" title="Delete" onClick="leaveDetail('${data}')" data-toggle="modal" data-target="#updateModal">
                                 <i class="fas fa-edit"></i>
                                 </button>
                                 <button type="button" class="btn btn-sm btn-danger" title="Delete" onClick="employeeDelete('Employee/Delete/${data}')" >
@@ -86,3 +86,20 @@ $(document).ready(function () {
         ]
     });
 });
+
+function leaveDetail(reqId) {
+    let data = {
+        request_id: reqId
+    }
+    console.log(data)
+    $.ajax({
+        url: "https://localhost:44302/api/leavingrequest/emp/detail",
+        data: JSON.stringify(data),
+        type: "GET",
+        contentType: 'application/json',
+    }).done(u => {
+        $("#request_id").val(u.request_id);
+        $("#employee_name").val(u.employee_id);
+        console.log(u)
+    })
+}
