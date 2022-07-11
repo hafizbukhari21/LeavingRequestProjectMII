8a308c9ee2c234d530cebbb02ecfa48970ea8d46
M Hafiz B <hafiz.bukhari@hotmail.com>
2022-07-11 11:50:47 +0700

    Timeline manager Chage timeline into apex chart

--- Client/wwwroot/js/indexEmp_manager.js
+++ Client/wwwroot/js/indexEmp_manager.js
@@ -1,5 +1,4 @@
-﻿google.charts.load('current', { 'packages': ['timeline'] });
-google.charts.setOnLoadCallback(drawChart);
+﻿drawChart()
 
 
 
@@ -27,96 +26,43 @@ $.ajax({
 
 
 function drawChart() {
-    console.log("callback " + idEmp)
+    //console.log("callback " + idEmp)
     $.ajax({
         type: "GET",
         url: `https://localhost:44302/api/leavingrequest/man/${idEmp}/calendar`,
 
         success: function (response) {
-            var container = document.getElementById('timeline');
-            var chart = new google.visualization.Timeline(container);
-            var dataTable = new google.visualization.DataTable();
-
-            console.log(response)
-
-            let rows = response.map(e => {
-                ConvertUTCToDate(e.startDate)
-                let colorStatus = ""
-                switch (e.status) {
-                    case "Menunggu":
-                        colorStatus = "#0275d8"
-                        break
-                    case "Diterima":
-                        colorStatus = "#5cb85c"
-                        break
-                    case "Ditolak":
-                        colorStatus = "#d9534f"
-                        break
-                    case "Revisi":
-                        colorStatus = "#f0ad4e"
-                        break
-                    case "Cancel":
-                        colorStatus = "#292b2c"
-                        break
+            //console.log(response)
+            let data = response.filter(e => e.status === "Diterima").map(e => {
+                let startDate = Math.floor(new Date(ConvertUTCToDate(e.startDate)).getTime() )
+                let endDate = Math.floor(new Date(ConvertUTCToDate(e.endDate)).getTime() )
+                return {
+                    x: e.nameEmployee,
+                    y: [startDate,endDate]
                 }
-                return [
-                    e.nameEmployee,
-                    e.status,
-                    colorStatus,
-                    new Date(ConvertUTCToDate(e.startDate)),
-                    new Date(ConvertUTCToDate(e.endDate)),
-                    `<h1>dfdf adasdsad as dsad sad sad </h1>`,
-
-
-                ]
             })
-
-            console.log(rows)
-
-            dataTable.addColumn({ type: 'string', id: 'Employee' });
-           
-            dataTable.addColumn({ type: 'string', id: 'Status' });
-            dataTable.addColumn({ type: 'string', role: 'style' });
-            dataTable.addColumn({ type: 'date', id: 'Start' });
-            dataTable.addColumn({ type: 'date', id: 'End' });
-            dataTable.addColumn({ type: 'string', role: 'tooltip', p: { html: true } });
-
-            dataTable.addRows(rows);
-
-
+            //console.log(data)
             var options = {
-                tooltip: { isHtml: true }
+                chart: {
+                    height: 300,
+                    type: 'rangeBar',
+                },
+                plotOptions: {
+                    bar: {
+                        horizontal: true
+                    }
+                },
+                xaxis: {
+                    type: 'datetime'
+                },
+                series: [{
+                    data
+                }],
             }
 
-            chart.draw(dataTable);
+            var chart = new ApexCharts(document.querySelector("#timeline"), options);
 
-
-            function myHandler(e) {
-                let startDate = ConvertUTCToDate(dataTable.getValue(e.row, 3))
-                let endDate = ConvertUTCToDate(dataTable.getValue(e.row, 4))
-                let duration = ((new Date(endDate) - new Date(startDate)) / (1000 * 3600 * 24))+1
-               
-                if (e.row != null) {
-                    
-                    $(".google-visualization-tooltip").html(`
-                        <div class="card" style="width: 18rem;">
-                              <div class="card-body">
-                                <h5 class="card-title">${dataTable.getValue(e.row, 1)}</h5>
-                                <p class="card-text">Tanggal Cuti dari ${startDate} s.d. ${endDate}</p>
-                                <p class="card-text">Durasi ${duration} Hari</p>
-                                
-                                </div>
-                            </div>
-
-                    `).css({ width: "auto", height: "auto" });;
-                }
-            }
-
-            google.visualization.events.addListener(chart, 'onmouseover', myHandler);
-
-            function customTooltip() {
-                return `<h1>dfdf adasdsad as dsad sad sad </h1>`
-            }
+            chart.render();
 
         }
     });
